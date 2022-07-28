ad59bb3fd34d503f63de25bb2a5d6b14e3b902e9
Benjabriel <83257668+Benjabriel@users.noreply.github.com>
2022-07-27 21:47:16 -0600

    agregue QR 2

--- QR/Generar.py
+++ QR/Generar.py
@@ -1,6 +1,36 @@
 from cgitb import html
+from email.mime import image
+from turtle import fillcolor
+from urllib.error import URLError
 import jinja2
 import pdfkit
+import qrcode
+from PIL import Image
+
+def Hacer_QR():
+    Logo_link="HTML/COFIDI.jpg"
+    logo=Image.open(Logo_link)
+    basewidth=100
+
+    wpercent=(basewidth/float(logo.size[0]))
+    hsize=int((float(logo.size[1])*float(wpercent)))
+    logo= logo.resize((basewidth,hsize),Image.ANTIALIAS)
+    QRCODE= qrcode.QRCode(
+    error_correction=qrcode.constants.ERROR_CORRECT_H
+    )
+    url="Hola"
+    QRCODE.add_data(url)
+    QRCODE.make()
+    QRCOLOR="Green"
+    Qrimg= QRCODE.make_image(
+        fillcolor=QRCOLOR,back_color='white'
+    ).convert('RGB')
+    
+    pos=((Qrimg.size[0]-logo.size[0])//2,
+        (Qrimg.size[1]-logo.size[1])//2)
+    Qrimg.paste(logo,pos)
+    Qrimg.save("QR_GUARDADO.png")
+    print("Se hizo el QR")
 
 def crear_pdf(ruta_template,info):
     nombre_template= ruta_template.split('/')[-1]
@@ -22,7 +52,7 @@ def crear_pdf(ruta_template,info):
     ruta_salida="r.pdf"
     pdfkit.from_string(html,ruta_salida,css="",options=option,configuration=config)
 
-
+Hacer_QR()
 if __name__=="__main__":
     ruta_template="HTML/index.html"
     info = {"ID":"1241241","Nombre":"Gabriel Valdez"}
